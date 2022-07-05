8bed001502c09d1b4be494eaca5f89815421c182
Daniel Hofmann <d.h0602@yahoo.de>
2022-07-05 17:08:35 +0200

    Koordinatenkreuz implementiert. Bug beim Zeichnen des Graphen.

--- src/geradeFrame.java
+++ src/geradeFrame.java
@@ -10,28 +10,36 @@ public class geradeFrame extends JFrame implements ActionListener {
     String x;
     String y;
 
+    String xs;
+
+    String ys;
+    String schritte;
+
     geradeFrame(){
+        xs = JOptionPane.showInputDialog("geben Sie das x-Achsenmaximum an: ");
+        ys = JOptionPane.showInputDialog("geben Sie das y-Achsenmaximum an: ");
+        schritte = JOptionPane.showInputDialog("geben Sie die Anzahl der Schritte an: ");
         x = JOptionPane.showInputDialog("geben Sie die Steigung an: ");
         y = JOptionPane.showInputDialog("geben Sie den y-Achsenabschnitt an: ");
-        panel = new geradeGraph(x,y);
-        JLabel yAchse = new JLabel("Y");
+        panel = new geradeGraph(x,y,xs,ys,schritte);
+        /*JLabel yAchse = new JLabel("Y");
         yAchse.setBounds(47,30,10,10);
         this.add(yAchse);
         JLabel xAchse = new JLabel("X");
         xAchse.setBounds(553,545,10,10);
-        this.add(xAchse);
+        this.add(xAchse);*/
 
         this.add(zurueck);
         Integer yas = 500;
-        for(int i = 50; i < 540; i = i+20){
+        /*for(int i = 50; i < 540; i = i+20){
             JLabel jl = new JLabel(yas.toString());
             yas = yas -20;
             jl.setBounds(20,i - 5,35,10);
             this.add(jl);
-        }
+        }*/
         Integer xas = 20;
         int a = 5;
-        for(int i = 60; i <= 540; i = i+20){
+        /*for(int i = 60; i <= 540; i = i+20){
             JLabel jl = new JLabel(xas.toString());
             xas = xas +20;
             if(i < 100){
@@ -42,7 +50,7 @@ public class geradeFrame extends JFrame implements ActionListener {
             }
             a = a*-1;
             this.add(jl);
-        }
+        }*/
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.add(panel);
         this.pack();
--- src/geradeGraph.java
+++ src/geradeGraph.java
@@ -4,38 +4,63 @@ import java.awt.*;
 public class geradeGraph extends JPanel {
     String M;
     String N;
-    geradeGraph(String m, String n){
+    String xs;
+    String ys;
+
+    String schritte;
+    geradeGraph(String m, String n, String XS, String YS, String Schritte){
         this.setPreferredSize(new Dimension(1000,800));
+
         M = m;
         N = n;
+        xs = XS;
+        ys = YS;
+        schritte = Schritte;
     }
 
     public void paint(Graphics g){
+        int w = this.getWidth()/2;
+        int h = this.getHeight()/2;
+        int xmax = Integer.parseInt(xs);
+        int ymax = Integer.parseInt(ys);
+        int anzSchritte = Integer.parseInt(schritte);
+
         Graphics2D graphics2D = (Graphics2D) g;
 
         graphics2D.setStroke(new BasicStroke(3)); //Strichdicke
 
         graphics2D.setPaint(Color.black);
 
-        graphics2D.drawLine(50,50,50,550); //Y-Achse
-        graphics2D.drawLine(50,550,550,550); //X-Achse
+        graphics2D.drawLine(w,0,w,h*2); //Y-Achse
+        graphics2D.drawLine(0,h,w*2,h); //X-Achse
 
         //graphics2D
         graphics2D.setStroke(new BasicStroke(1));
-        for (int i = 530; i > 60; i = i-20){
-            graphics2D.drawLine(i,545,i,555);
+        //x-Achsenbeschriftung
+        for (int i = w; i <= 2*w; i = i + w/anzSchritte){
+            graphics2D.drawLine(i,h-5,i,h+5);
         }
-        for (int i = 530; i> 60; i = i-20){
-            graphics2D.drawLine(45,i,55,i);
+        for (int i = w; i >= 0; i = i - w/anzSchritte){
+            graphics2D.drawLine(i,h-5,i,h+5);
+        }
+
+        //y-Achsenbeschriftung
+        for (int i = h; i <= 2*h; i = i + h/anzSchritte){
+            graphics2D.drawLine(w-5,i,w+5,i);
+        }
+        for (int i = h; i >= 0; i = i - h/anzSchritte){
+            graphics2D.drawLine(w-5,i,w+5,i);
         }
 
 
 
         int steigung = Integer.parseInt(M);
         int abschnitt = Integer.parseInt(N);
-
+        //double a  = h-(abschnitt * (h / anzSchritte));
+        double a = steigung * (w /anzSchritte);
+        double b = steigung * (w / anzSchritte);
         graphics2D.setStroke(new BasicStroke(2));
-        graphics2D.drawLine(50,550-abschnitt,550,550-(steigung*500));
+        graphics2D.drawLine(0,h + (int)a,w*2,(int)b);
 
     }
 }
